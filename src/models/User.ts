06144c87c384520a18f84691f92e0ff17e5f48cd
@danilko.a.g <danilko.a.g@gmail.com>
2025-08-19 22:16:14 +0300

    add user model

---
+++ src/models/User.ts
@@ -0,0 +1,49 @@
+import { Table, Column, Model, DataType, HasMany, CreatedAt, UpdatedAt } from 'sequelize-typescript';
+import { Post } from './Post';
+
+@Table({
+  tableName: 'users',
+  timestamps: true,
+})
+export class User extends Model {
+  @Column({
+    type: DataType.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  })
+  id!: number;
+
+  @Column({
+    type: DataType.STRING(100),
+    allowNull: false,
+    unique: true,
+  })
+  email!: string;
+
+  @Column({
+    type: DataType.STRING(100),
+    allowNull: false,
+  })
+  password!: string;
+
+  @Column({
+    type: DataType.STRING(50),
+    allowNull: false,
+  })
+  firstName!: string;
+
+  @Column({
+    type: DataType.STRING(50),
+    allowNull: false,
+  })
+  lastName!: string;
+
+  @CreatedAt
+  createdAt!: Date;
+
+  @UpdatedAt
+  updatedAt!: Date;
+
+  @HasMany(() => Post)
+  posts!: Post[];
+}
